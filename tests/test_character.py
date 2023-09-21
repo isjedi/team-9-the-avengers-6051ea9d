8b761bc6dd65b5871f54de9e8839d3cd713fedfa
Jahelle <jahellecato98@gmail.com>
2023-09-21 15:34:27 +0000

    test for character initilization

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,11 +1,13 @@
-import pytest
-from src.levelup.character import Character
+from unittest import TestCase
+# import pytest
+from levelup.character import Character
 
-class Character:
+class Character(TestCase):
     DEFAULT_NAME: str = "Jane Doe"
     name_from_user: str = "Tierna"
 
-    game_character = Character()
+    game_character_default_name = Character()
+    game_character_user_name = Character(name_from_user)
 
     def initialize_character_with_default_name(self, DEFAULT_NAME):
         self.DEFAULT_NAME = DEFAULT_NAME
@@ -13,11 +15,11 @@ class Character:
     def initialize_character_name_from_user(self, name_from_user):
         self.name_from_user = name_from_user
 
-    def test_default_name(self, expected):
-        name = self.game_character.name
+    def test_default_name(self):
+        name = self.game_character_default_name.name
         assert name == self.DEFAULT_NAME
     
-    def test_character_name(self, expected):
-        name = self.game_character.name
+    def test_character_name(self):
+        name = self.game_character_user_name.name
         assert name == self.name_from_user
         
