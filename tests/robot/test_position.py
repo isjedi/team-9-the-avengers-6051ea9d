3583138186615634aec2fb15017683dcda224375
skippy <kenneth.hicks@carolina.rr.com>
2023-09-21 16:03:08 +0000

    update to test coordinate

--- tests/robot/test_position.py
+++ tests/robot/test_position.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 from levelup.position import Position
 
 class TestPosition(TestCase):
+    TEST_XYCOORDINATES = (1,1)
+
     def test_init(self):
         TEST_XYCOORDINATES = (1,1)
         testobj = Position()
