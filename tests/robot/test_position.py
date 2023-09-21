91ea422316fe5e010d1d86e23c5e80ae917107e7
skippy <kenneth.hicks@carolina.rr.com>
2023-09-21 16:04:15 +0000

    updated coordinates

--- tests/robot/test_position.py
+++ tests/robot/test_position.py
@@ -5,7 +5,7 @@ class TestPosition(TestCase):
     TEST_XYCOORDINATES = (1,1)
 
     def test_init(self):
-        TEST_XYCOORDINATES = (1,1)
+        TEST_XYCOORDINATES = (0,0)
         testobj = Position()
         assert testobj.coordinates[0] == self.TEST_XYCOORDINATES[0]
         assert testobj.coordinates[1] == self.TEST_XYCOORDINATES[1]
