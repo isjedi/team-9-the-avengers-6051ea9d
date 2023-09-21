d067262bb03176f7e883158d7025189091f56b62
Toby Andrews <dsjedi@gmail.com>
2023-09-21 20:25:27 +0000

    removed unnessecary tests

--- tests/test_position.py
+++ tests/test_position.py
@@ -9,13 +9,3 @@ class TestPosition(TestCase):
         testobj = Position()
         assert testobj.coordinates[0] == self.TEST_XYCOORDINATES[0]
         assert testobj.coordinates[1] == self.TEST_XYCOORDINATES[1]
-
-    def test_pass(self):
-        testobj = Position(NEW_POSITION)
-        assert testobj.coordinates[0] == self.NEW_POSITION[0]
-        assert testobj.coordinates[1] == self.NEW_POSITION[1]
-
-    def test_set_position(self):
-        testobj = Position.Position(self.NEW_POSITION)
-        assert testobj[0] == 5
-        
