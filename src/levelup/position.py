f811c83f99e26d4a23253f085cc806f192e7dae9
Toby Andrews <dsjedi@gmail.com>
2023-09-21 20:05:21 +0000

    who knows what is going on

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -3,10 +3,7 @@ class Position:
     # random ints in a bound range of 0-9 use random.randint(0,9)
 
     def _init_(self, coordinates: tuple):
-        self.xcoordinate = coordinates[0]
-        self.ycoordinate = coordinates[1]
-        self.coordinates = (self.xcoordinate, self.ycoordinate)
+        #self.xcoordinate = coordinates[0]
+        #self.ycoordinate = coordinates[1]
+        self.coordinates = coordinates #(self.xcoordinate, self.ycoordinate)
 
-    def Position(coordinates):
-        newCoordinates = coordinates
-        return newCoordinates
--- tests/test_position.py
+++ tests/test_position.py
@@ -2,13 +2,18 @@ from unittest import TestCase
 from levelup.position import Position
 
 class TestPosition(TestCase):
-    TEST_XYCOORDINATES = (10,10)
-    NEW_POSITION = (5,5)
+    TEST_XYCOORDINATES = (1,1)
+    NEW_POSITION = (5,7)
 
     def test_init(self):
         testobj = Position()
         assert testobj.coordinates[0] == self.TEST_XYCOORDINATES[0]
         assert testobj.coordinates[1] == self.TEST_XYCOORDINATES[1]
+
+    def test_pass(self):
+        testobj = Position(NEW_POSITION)
+        assert testobj.coordinates[0] == self.NEW_POSITION[0]
+        assert testobj.coordinates[1] == self.NEW_POSITION[1]
 
     def test_set_position(self):
         testobj = Position.Position(self.NEW_POSITION)
