055513da4a1ffe5a7668e191111167c387c7ec4c
Toby Andrews <dsjedi@gmail.com>
2023-09-21 18:20:02 +0000

    working on method

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,9 +1,10 @@
 class Position:
     coordinates: tuple = (0, 0)
-    xCoordinate: int = 1
-    yCoordinate: int = 1
 
     def _init_(self, coordinates: tuple):
         self.xcoordinate = coordinates[0]
         self.ycoordinate = coordinates[1]
         self.coordinates = (self.xcoordinate, self.ycoordinate)
+
+    def Position()
+        self.coordinates = coordinates
