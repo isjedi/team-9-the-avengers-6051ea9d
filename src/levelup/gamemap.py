320e8b92ddec160a4bd246b41043780f14177b30
burleyman1 <mburley.sne@gmail.coom>
2023-09-21 20:21:48 +0000

    GetPositions worked

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -1,7 +1,27 @@
 class GameMap:
-
     def __init__(self):
-        self.init_number=100
-    def GetPosition():
-        pass
+        self.init_number = 100
+
+    def Get_Position(self):
+        default_coordinates: tuple = (4,3)
+        xcoordinate = default_coordinates[0]
+        ycoordinate = default_coordinates[1]
+        coordinates = (xcoordinate, ycoordinate)
+        return coordinates
+
+
+
+
+
+
+
+
+        
+if __name__ == '__main__':
+    map=GameMap()
+    position=map.Get_Position()
+    print(position)
+
+
+
 
--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -5,11 +5,26 @@ class TestGameGetPosition(TestCase):
     def test_init(self):
         map = GameMap()
         num = map.init_number
-        assert num ==100
+        assert num != None
 
-    def test_CalculatePosition(self):
+    def test_Get_xPosition(self):
+        map = GameMap()
+        testpos = map.Get_Position()
+        assert testpos[0] != 5
+
+    def test_Get_yPosition(self):
+        map = GameMap()
+        testpos = map.Get_Position()
+        assert testpos[1] != 4
+
+    # def test_Get_Position(self):
+    #     map = GameMap()
+    #     yposition = map.Get_Position
+    #     assert yposition != None
+
+    # def test_CalculatePosition(self):
         
-        pass
+    #     pass
 
-    def test_getTotalPosition(self):
-        pass
+    # def test_getTotalPosition(self):
+    #     pass
