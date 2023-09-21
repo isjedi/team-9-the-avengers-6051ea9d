fcad002428904cafe382552b1aa302241e6b8e36
Toby Andrews <dsjedi@gmail.com>
2023-09-21 20:34:44 +0000

    added other test

--- tests/test_position.py
+++ tests/test_position.py
@@ -5,7 +5,10 @@ class TestPosition(TestCase):
     TEST_XYCOORDINATES = (1,1)
     NEW_POSITION = (5,7)
 
-    def test_init(self):
+    def test_init_xcoordinate(self):
         testobj = Position()
         assert testobj.coordinates[0] == self.TEST_XYCOORDINATES[0]
+
+    def test_init_ycoordinate(self):
+        testobj = Position()
         assert testobj.coordinates[1] == self.TEST_XYCOORDINATES[1]
