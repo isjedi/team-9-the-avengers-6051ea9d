636b7eec7808f51f3e4b6e886f0f75c02e099e81
Toby Andrews <dsjedi@gmail.com>
2023-09-21 20:45:31 +0000

    changed tuple to list

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,8 +1,8 @@
 class Position:
-    coordinates: tuple = (1, 1)
+    coordinates: list = (1, 1)
     # random ints in a bound range of 0-9 use random.randint(0,9)
 
-    def _init_(self, coordinates: tuple):
+    def _init_(self, coordinates: list):
         #self.xcoordinate = coordinates[0]
         #self.ycoordinate = coordinates[1]
         self.coordinates = coordinates #(self.xcoordinate, self.ycoordinate)
