b326cdd20f3837958b22dcdcb4bbbc8c49c666ec
Tingyu <tingyushi0@gmail.com>
2023-09-21 15:47:41 +0000

    initial test_gamemap.py

---
+++ src/levelup/gamemap.py
@@ -0,0 +1,7 @@
+class GameMap:
+
+    def __init__(self):
+        self.init_number=100
+    def GetPosition():
+        pass
+
---
+++ tests/test_gamemap.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from levelup.gamemap import GameMap
+
+class TestGameGetPosition(TestCase):
+    def test_init(self):
+        map = GameMap()
+        num = map.init_number
+        assert num ==100
+
+    def test_CalculatePosition(self):
+        
+        pass
+
+    def test_getTotalPosition(self):
+        pass
