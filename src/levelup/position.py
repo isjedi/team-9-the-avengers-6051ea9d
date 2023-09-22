624eee694be9e52ef663b6d41d17da88f1729adc
Toby Andrews <dsjedi@gmail.com>
2023-09-22 14:45:09 +0000

    updates Position class and test wadded set method

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,9 +1,13 @@
 class Position:
-    coordinates: list = [1, 1]
+    coordinates: list = [0,0]
     # random ints in a bound range of 0-9 use random.randint(0,9)
 
-    def _init_(self, coordinates: list):
-        #self.xcoordinate = coordinates[0]
-        #self.ycoordinate = coordinates[1]
+    def __init__(self, coordinates: list):
         self.coordinates = coordinates #(self.xcoordinate, self.ycoordinate)
 
+    def setPosition(self, xCoordinates, yCoordinates):
+        self.coordinates[0] = xCoordinates
+        self.coordinates[1] = yCoordinates
+
+    def __str__(self):
+        return f"coords: {self.coordinates}"
--- tests/test_position.py
+++ tests/test_position.py
@@ -2,13 +2,32 @@ from unittest import TestCase
 from levelup.position import Position
 
 class TestPosition(TestCase):
-    TEST_XYCOORDINATES = [1,1]
-    NEW_POSITION = [5,7]
+    TEST_XYCOORDINATES: list = [1,1]
+    NEW_POSITION_X: int = 5
+    NEW_POSITION_Y: int = 7
 
     def test_init_xcoordinate(self):
-        testobj = Position()
-        assert testobj.coordinates[0] == self.TEST_XYCOORDINATES[0]
+        TEST_XYCOORDINATES = [1,1]
+        testobj1 = Position(TEST_XYCOORDINATES)
+        assert testobj1.coordinates[0] == self.TEST_XYCOORDINATES[0]
 
     def test_init_ycoordinate(self):
-        testobj = Position()
-        assert testobj.coordinates[1] == self.TEST_XYCOORDINATES[1]
+        TEST_XYCOORDINATES = [1,1]
+        testobj2 = Position(TEST_XYCOORDINATES)
+        assert testobj2.coordinates[1] == self.TEST_XYCOORDINATES[1]
+
+    def test_Position_xcoordinate(self):
+        TEST_XYCOORDINATES = [1,1]
+        NEW_POSITION_X = 5
+        NEW_POSITION_Y = 7
+        testobj3 = Position(TEST_XYCOORDINATES)
+        testobj3.setPosition(NEW_POSITION_X, NEW_POSITION_Y)
+        assert testobj3.coordinates[0] == NEW_POSITION_X
+
+    def test_Position_ycoordinate(self):
+        TEST_XYCOORDINATES = [1,1]
+        NEW_POSITION_X = 6
+        NEW_POSITION_Y = 8
+        testobj3 = Position(TEST_XYCOORDINATES)
+        testobj3.setPosition(NEW_POSITION_X, NEW_POSITION_Y)
+        assert testobj3.coordinates[1] == NEW_POSITION_Y
